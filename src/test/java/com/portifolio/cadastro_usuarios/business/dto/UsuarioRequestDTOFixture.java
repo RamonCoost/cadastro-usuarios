f6eaccfe246d1973eb516f4621560f81f11ef7df
RamonCoost <ramoncoost@gmail.com>
2026-05-06 20:15:35 -0300

    refactor: adiciona build padrao nos fixtures de dto

--- src/test/java/com/portifolio/cadastro_usuarios/business/dto/UsuarioRequestDTOFixture.java
+++ src/test/java/com/portifolio/cadastro_usuarios/business/dto/UsuarioRequestDTOFixture.java
@@ -2,6 +2,10 @@ package com.portifolio.cadastro_usuarios.business.dto;
 
 public class UsuarioRequestDTOFixture {
 
+    public static UsuarioRequestDTO build() {
+        return build("Ramon", "ramon@email.com", "123456");
+    }
+
     public static UsuarioRequestDTO build(String nome,
                                           String email,
                                           String senha) {
