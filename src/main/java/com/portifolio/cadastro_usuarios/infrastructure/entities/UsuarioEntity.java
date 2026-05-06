adfcdab467071d731a04b14a7ecc7d458abe1dd2
RamonCoost <ramoncoost@gmail.com>
2026-05-06 06:56:01 -0300

    feat: cria entidade e repository de usuario

---
+++ src/main/java/com/portifolio/cadastro_usuarios/infrastructure/entities/UsuarioEntity.java
@@ -0,0 +1,30 @@
+package com.portifolio.cadastro_usuarios.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "usuarios")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+}
